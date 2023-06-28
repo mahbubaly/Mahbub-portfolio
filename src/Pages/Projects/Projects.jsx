6f9a141c11c306cf67daf270ab2842d41b2acd9c
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-06-28 23:05:46 +0600

    added-more-projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -5,7 +5,8 @@ import img3 from '../../assets/Images/Projects/a10.png'
 import img4 from '../../assets/Images/Projects/food.png'
 import img5 from '../../assets/Images/Projects/appliedJon.png'
 import img6 from '../../assets/Images/Projects/127.0.0.1_5500_index.html.png'
-import img7 from '../../assets/Images/Projects//a3.png'
+import img7 from '../../assets/Images/Projects/a3.png'
+import img8 from '../../assets/Images/Projects/bistro.png'
 import '../../Pages/Style.css'
 import 'aos/dist/aos.css';
 import { motion } from "framer-motion"
@@ -64,6 +65,47 @@ const Projects = () => {
                                             </a>
                                             <a href='https://github.com/programming-hero-web-course1/b7a12-summer-camp-server_side-mahbubaly' target='_blank' className='flex items-center gap-2 tooltip' data-tip="Server Site Code">Server <FaGithub className='w-8 h-8' /></a>
                                             <a href='https://summer-camp-d0cd9.web.app' target='_blank' className='flex items-center gap-2 tooltip' data-tip="Live Site">Live <FaLink className='w-8 h-8'></FaLink></a>
+                                        </div>
+                                        <div className="card-actions justify-end">
+                                            <button className="btn btn-primary mt-2" onClick={() => window.my_modal_5.showModal()}>Explore more</button>
+                                        </div>
+                                    </div>
+                                </motion.div>
+
+                                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                                    <form method="dialog" className="modal-box">
+                                        <h3 className="font-bold text-lg">Hello!</h3>
+                                        <p className="py-4">This is Popular Chefs And Foods web site.
+                                            This is a single page web site
+                                            Use this Site you can Choose Chef And Food.
+                                            You see the many Chefs and every single Chef details .
+                                            You can see the Chefs Recipes , Chefs Like , Chefs Experience and Number Of Recipes.
+                                            You Also Create your Account And then Login this site .</p>
+                                        <div className="modal-action">
+
+                                            <button className="btn">Close</button>
+                                        </div>
+                                    </form>
+                                </dialog>
+                            </div>
+
+                            <div className='text-white'>
+                                <motion.div
+                                    variants={fadeIn('right', 0.5)}
+                                    initial={'hidden'}
+                                    whileInView={'show'}
+                                    viewport={{ once: false, amount: 0.3 }}
+                                    className="card w-full glass">
+                                    <div className='overflow-auto h-60  rounded-lg'  ><img className='object-contain h-fit p-3  ' src={img8} alt="summer-training!" /></div>
+
+                                    <div className="card-body">
+                                        <h2 className="card-title">Bistro-boss</h2>
+
+                                        <div className='flex items-center justify-between'>
+                                            <a href='https://github.com/mahbubaly/bistro-boss-res' target='_blank' className='flex items-center gap-2 tooltip' data-tip="Client Site Code">Client <FaGithub className='w-8 h-8' />
+                                            </a>
+                                            <a href='' target='_blank' className='flex items-center gap-2 tooltip' data-tip="Server Site Code">Server <FaGithub className='w-8 h-8' /></a>
+                                            <a href='https://lucky-biscotti-f144d5.netlify.app' target='_blank' className='flex items-center gap-2 tooltip' data-tip="Live Site">Live <FaLink className='w-8 h-8'></FaLink></a>
                                         </div>
                                         <div className="card-actions justify-end">
                                             <button className="btn btn-primary mt-2" onClick={() => window.my_modal_5.showModal()}>Explore more</button>
@@ -169,12 +211,6 @@ const Projects = () => {
                                     </form>
                                 </dialog>
                             </div>
-
-                        </div>
-
-                        {/* 2 portion */}
-
-                        <div className='grid gap-10 md:grid-cols-2 lg:grid-cols-3 grid-cols-1 mt-12'>
 
                             <div className='text-white'>
                                 <motion.div
@@ -298,6 +334,7 @@ const Projects = () => {
                                     </form>
                                 </dialog>
                             </div>
+
                             <div className='text-white'>
                                 <motion.div
                                     variants={fadeIn('right', 0.5)}
@@ -339,10 +376,10 @@ const Projects = () => {
                                 </dialog>
                             </div>
 
-
-
-
                         </div>
+
+                        
+
 
                     </div>
 
